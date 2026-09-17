db01b2163c09e8edd8f53e8d8330adf6e0582fda
grayhex <grayhex@users.noreply.github.com>
2026-09-17 22:09:38 +0300

    Add showcase persistence, deterministic scoring and vote protections

--- scripts/test-db.js
+++ scripts/test-db.js
@@ -25,6 +25,7 @@ await db.query("INSERT INTO site_catalog(id,value) VALUES(1,$1)", [
 await db.exec(await readFile(new URL("../db/004_garage_layout.sql",import.meta.url),"utf8"));
 await db.exec(await readFile(new URL("../db/005_bike_wizard.sql",import.meta.url),"utf8"));
 await db.exec(await readFile(new URL("../db/006_compact_defaults.sql",import.meta.url),"utf8"));
+await db.exec(await readFile(new URL("../db/007_showcase.sql",import.meta.url),"utf8"));
 const server = new PGLiteSocketServer({
   db,
   host: "127.0.0.1",
